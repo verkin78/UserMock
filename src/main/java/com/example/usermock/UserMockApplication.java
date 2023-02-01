41d9273f33eff99b91d9cd85775aa0264b854e11
Verkin78 <otrajenie.jc@gmail.com>
2023-02-01 21:47:13 +0300

    UserMock - the project created

---
+++ src/main/java/com/example/usermock/UserMockApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usermock/UserMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usermock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
